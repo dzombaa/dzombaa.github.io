9a9ec62b5a0ccfab2a56ed7e8c2b809b829421a1
dzombaa <amardzomba@gmail.com>
2016-04-19 00:37:19 -0400

    ..

--- hlpscrpt.js
+++ hlpscrpt.js
@@ -10,6 +10,6 @@ script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-$( $.parseHTML( "<span class=okwhen> It's ok to help when </span>" ) ).prependTo( ".answer" );
+$( $.parseHTML( "<span class="okwhen"> It's ok to help when </span>" ) ).prependTo( ".answer" );
 /**
 $( $.parseHTML( "<b> It's ok to help when </b>" ) ).appendTo( ".answer" );*/

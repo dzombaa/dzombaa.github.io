2d2479ed84dd245b52d057719148a3360209281c
dzombaa <amardzomba@gmail.com>
2016-04-19 00:10:15 -0400

    .

--- hlpscrpt.js
+++ hlpscrpt.js
@@ -1,6 +1,8 @@
 /**$("<span> It's ok to help when..</span>").before(".answer");
 
 $( $.parseHTML( "<span class="okwhen"> It's ok to help when </span>" ) ).prependTo( ".answer" );
-*/
 
-$( $.parseHTML( "<span> It's ok to help when </span>" ) ).prependTo( ".answer" );
+
+$( $.parseHTML( "<span> It's ok to help when </span>" ) ).prependTo( ".answer" );*/
+
+$( $.parseHTML( "<b> It's ok to help when </b>" ) ).appendTo( ".answer" );
